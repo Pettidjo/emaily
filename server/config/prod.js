ee718e486fc3cbb968676f4498b2327acb2a0b03
Jonathan Herbault <jonathanherbault@gmail.com>
2018-10-15 17:39:57 +0200

    initial commit

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongooseConnect: process.env.MONGOOSE_CONNECT,
+  cookieKey: process.env.COOKIE_KEY,
+}
